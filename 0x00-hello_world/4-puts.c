703550f16e547fbc699bd497f2abeb2d47baef9d
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-10 17:35:54 +0000

    My fourth commit task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #!/bin/bash
-#include <stdio.h>
-	int main(void){
-		print("Programming is like building a multilingua puzzle");
-		return 0;
+#include<studio.h>
+     int main (void){
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return 0;
 }
