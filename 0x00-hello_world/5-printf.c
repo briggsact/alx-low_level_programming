b56e7b678e7b8993681d32caee3f37aa3506ce19
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-10 22:19:37 +0000

    My fifth task commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
-#!/bin/bash
-#include<stdio.h>
-int main (void)
+#inncle <stdio.h>
+/**
+ *main -entry point
+ *
+ *Return: 0 after printing the fuction
+ */
+int main(void)
 {
-    printf("with proper grammer, but the outcome is a piece of art\n") ;
-    return (0) ;
+printf("with proper grammar, but the outcome is a piece of art\n");
+return (0);
 }
