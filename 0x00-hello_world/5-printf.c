ff83055bbca47a3754ac29efc2a68875cf3bb9f8
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-10 23:40:49 +0000

    My fifth commit task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  *main -Entry point
  *My hello, printf task
- *Return: return 1 after program execution
+ *Return: return  1 after program execution
  */
 int main(void)
 {
