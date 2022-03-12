5f44c9fa45c33cf95d4dedfb77576147264a096b
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-12 11:23:55 +0000

    My zero task commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-int n;
+vvint n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
@@ -21,6 +21,11 @@ printf("%i is zero\n", n);
 }
 
 else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
+
+else
 {
 printf("%i is positive\n", n);
 }
