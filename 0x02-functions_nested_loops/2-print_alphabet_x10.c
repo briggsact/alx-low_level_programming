0aa6ceee66740bb6419c96626b2749b41fa8cd46
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-16 02:52:21 +0000

    My second task commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,27 @@
-#include main
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Alaways 0.
+ */
+void print_alphabet_x10(void)
+{
+int alphabet;
+int count:
+
+
+count = 0;
+while (count < 10)
+{
+for (alphabet = a' ; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+count++;
+_putchar('\n');
+}
+
+
+}
