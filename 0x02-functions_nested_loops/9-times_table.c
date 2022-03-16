306cfb609a33c9e18f30f35fe4266f84330a88e0
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-16 05:54:54 +0000

    My ninth task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ if ((c / 10) == 0)
 {
 if (b == 0)
 {
-_putchar ('0')
+_putchar ('0');
 }
 if (b != 0)
 {
@@ -28,7 +28,7 @@ _putchar ((c % 10) + '0');
 if (b < 9)
 {
 _putchar(',');
-_putchar (' ");
+_putchar (' ');
 }
 }
 else
