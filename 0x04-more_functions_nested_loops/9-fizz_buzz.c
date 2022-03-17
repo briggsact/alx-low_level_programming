340aa0a399869cce784779d33dfa7b7e4f58f54e
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-17 19:39:52 +0000

    My ninth task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+
+int p = 100;
+int i;
+
+i = 1;
+while (i <= p)
+{
+if (i % 3 == 0 && i % 5 ==0)
+{
+printf("Fizzbuzz ");
+}
+
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+
+else if (i % 5 == 0)
+{
+if (i < p)
+printf("Buzz");
+}
+
+else
+{
+printf('%i ", i);
+}
+
+i++;
+
+
+}
+printf("\n");
+return (0);
+}
