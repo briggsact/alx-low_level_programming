1fb4851d6af9dc81d4a1bf81a8e8d583e31c11b6
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-21 18:43:36 +0000

    My commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
  */
 int is_numerical(unsigned int n)
 {
-return (n >= '0' && n <= '9');
+return (n >= '0' &&  n <= '9');
 }
 
 /**
@@ -41,7 +41,7 @@ sign *= -1;
 
 }
 
-return (number *sign*);
+return (number *sign);
 
 
 }
