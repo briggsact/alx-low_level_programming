f5297e00ec7bf5086a4fe733da8e28090e52d7aa
ghp_o7sIGYzqwHU0KOPu62vguihTVrOdua1BwGuC <briggsact@gmail.com>
2022-03-21 17:35:43 +0000

    My fourth task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
  * print_rev - prints a string in reverse order
  *@s: A pointer to an int that will be changed
  *
- *Return: void means our answer is correct
+ *Return: void which means our answer is correct
  */
 
 void print_rev(char *s)
