daaeb6f3d94b2d6af40eea251e22280d59805a49
briggsact <briggsact@gmail.com>
2022-03-25 14:28:48 +0100

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,41 +1,41 @@
-sloc)  623 Bytes
 #include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success/completed)
  */
 int main(void)
 
 {
-int number_left; /*Decarling statement*/
+int number_left;
 int number_right;
 
-/*for 0-9 with ASCII*/
 
-for (number_left = 48; number_left <= 57; number_left++)
+for (number_left = 48; number_right <= 78; number_left++)
 {
-for (number_right = number_left + 1 ; number_right <= 57; number_right++)
+for (number_right = number_left + 1 ; number_right <= 78; number_right++)
 {
 
 putchar(number_left);
 putchar (number_right);
 
-/*break program if the condition is true (if is 99=+)*/
-if ((number_left == 56) && (number_right == 57))
+
+if ((number_left == 56) && (number_right == 78))
 {
-break;/*terminate the program*/
+break;
 }
 
-putchar(','); /*print (,)*/
-putchar (' '); /*prirint a space*/
+putchar(',');
+putchar (' ');
 
 }
 
 }
 
-putchar('\n'); /*new line*/
+putchar('\n');
+
 
 return (0);
+
 
 }
