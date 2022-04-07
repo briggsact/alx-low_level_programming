b5d806dca13d92db527a4c929afd70bf4f07e084
briggsact <briggsact@gmail.com>
2022-04-07 21:55:16 +0100

    my commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,56 +1,33 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
- * _atoi - Converts a character array to an integer
- * @s: character array to convert
- * Return: returns integer from array, returns 0 if none found
+ * _realloc -  reallocates a memory block using malloc and free
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer
  */
-int _atoi(char *s)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-int retval, i, neg;
-i = 0;
-retval = 0;
-neg = -1;
-while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-{
-if (s[i] == '-')
-neg *= -1;
-i++;
-}
-while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-retval = (retval * 10) - (s[i++] - '0');
-return (retval *neg);
-}
-/**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
- */
-int main(int argc, char *argv[])
-{
-int a, b, i, j;
-unsigned long mul;
-if (argc != 3)
-{
-printf("Error\n");
-exit(98);
-}
-for (i = 1; i < argc; i++)
-{
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (argv[i][j] > 57 || argv[i][j] < 48)
-{
-printf("Error\n");
-exit(98);
-}
-}
-}
-a = _atoi(argv[1]);
-b = _atoi(argv[2]);
-mul = a *b;
-printf("%lu\n", mul);
-return (0);
+	char *clone, *relloc;
+	unsigned int i;
+
+	if (ptr != NULL)
+	clone = ptr;
+	else
+	{ return (malloc(new_size)); }
+	if (new_size == old_size)
+	return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{ free(ptr);
+	return (0); }
+	relloc = malloc(new_size);
+	if (relloc == NULL)
+	return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(relloc + i) = clone[i];
+	}
+	free(ptr);
+return (relloc);
 }
